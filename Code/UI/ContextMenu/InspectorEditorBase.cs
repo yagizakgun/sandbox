f427f83f22669e83ba456b1646fcd3f6cef22919
Tony Ferguson <devultj@gmail.com>
2026-03-24 23:21:00 +0000

    Use tab bar instead of separate windows cluttering the screen up

--- Code/UI/ContextMenu/InspectorEditorBase.cs
+++ Code/UI/ContextMenu/InspectorEditorBase.cs
@@ -6,20 +6,5 @@ public abstract class InspectorEditorBase : Panel
 {
 	public abstract bool TrySetTarget( List<GameObject> selection );
 
-	public bool IsExpanded
-	{
-		get => _isExpanded;
-		set { _isExpanded = value; StateHasChanged(); }
-	}
-	bool _isExpanded;
-
-	public bool ShowAccordion
-	{
-		get => _showAccordion;
-		set { _showAccordion = value; StateHasChanged(); }
-	}
-	bool _showAccordion;
-
-	public Action OnToggleExpanded { get; set; }
-	public void ToggleExpanded() => OnToggleExpanded?.Invoke();
+	public virtual string TabTitle => TypeLibrary.GetType( GetType() )?.Title ?? GetType().Name;
 }
